603ea5f5706719cfdce8dfac59ddad7ba84b9681
@rijswij2 <jaden.van.rijswijk@hva.nl>
2023-06-24 00:02:28 +0200

    Changed gitignore to ignore node modules and added a hello world program for the backend server

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,10 @@
 let express = require('express');
 let app = express();
+let port = 3001;
 
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+app.get('/', (req, res) => {
+  res.send('Hello World from the server!');
+})
 
